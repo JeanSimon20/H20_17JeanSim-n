8de32900c4a8bac22fe0bcddba22fa2c219ffbb9
JOSEPHSALAS <joseph.salas@vallegrande.edu.pe>
2020-09-29 20:19:36 -0500

    cambios

---
+++ Views/dbo.ListarProducto.sql
@@ -0,0 +1,10 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+--Listar los datos de los Productos
+CREATE VIEW [dbo].[ListarProducto]
+AS
+SELECT * FROM Producto
+
+GO
---
+++ Views/dbo.ListarSucursal.sql
@@ -0,0 +1,10 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+-- Lista todas las sucursales
+CREATE VIEW [dbo].[ListarSucursal]
+AS
+SELECT * FROM Sucursal
+
+GO
